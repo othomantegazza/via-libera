3d4d4c78aaa86695f6c22a5386a557b0c34bc0fd
Otho Mantegazza <otho.mantegazza@gmail.com>
2024-06-22 12:56:11 +0200

    explore correlations

--- correlations.R
+++ correlations.R
@@ -6,15 +6,162 @@ library(GGally)
 
 source("setup-viz.R")
 
-dat_cors <- 
+# read data -----------------------------------------------------
+
+dat <- 
   read_excel(
     "data/240614_carcount_ptal_population_poi.xlsx"
-  )
+  ) %>% 
+  clean_names()
 
 
-dat_cors %>%
-  select(-name, -id) %>% 
-  ggpairs(mapping = aes(alpha = .1))
+vie_e_municipi <- 
+  read_excel(
+    "data/VIA LIBERA!!!_Max.xlsx",
+    sheet = "VIA LIBERA_municipi"
+  ) %>% 
+  clean_names() %>% 
+  select(name, municipio, perc_municipio)
+
+# summarise buffer ---------------------------------------------
+
+dat_buffer <- 
+  dat %>% 
+  summarise(
+    total_car_number_per_100m = total_car_number_per_100m %>% 
+      mean(na.rm = T),
+    sum_of_population_in_100m_buffer = sum_of_population_in_100m_buffer %>% 
+      mean(na.rm = T),
+    weight = n(),
+    .by = name
+  ) 
+
+dat_buffer %>%  
+  uncount(weight) %>% 
+  drop_na() %>% 
+  {
+    cor(
+      .$total_car_number_per_100m,
+      .$sum_of_population_in_100m_buffer,
+      method = "spearman"
+    )
+  }
+
+dat_buffer %>% 
+  uncount(weight) %>%
+  filter(total_car_number_per_100m < 25) %>% 
+  ggplot() +
+  aes(x = sum_of_population_in_100m_buffer,
+      y = total_car_number_per_100m) +
+  geom_point(shape = 1,
+             alpha = .1)
+
+correlazione_municipi <- 
+  dat_buffer %>% 
+  left_join(vie_e_municipi) %>% 
+  mutate(weight = weight*perc_municipio %>% round()) %>% 
+  drop_na() %>% 
+  uncount(weight) %>% 
+  mutate(municipio = glue::glue("Municipio {municipio}")) %>% 
+  ggplot() +
+  aes(x = sum_of_population_in_100m_buffer,
+      y = total_car_number_per_100m) +
+  geom_point(shape = 1,
+             alpha = .1) +
+  geom_text(
+    data = . %>% 
+      summarize(
+        cor = cor(
+          total_car_number_per_100m,
+          sum_of_population_in_100m_buffer,
+          method = "spearman"
+        ) %>% 
+          round(2),
+        .by = municipio
+      ),
+      aes(
+        x = 4000,
+        y = 90,
+        label = paste("ρ =", cor)
+      ),
+    size = font_size/(1.5*size_scale)
+  ) +
+  scale_y_log10() +
+  facet_wrap(facets = "municipio")
+
+ggsave(
+  "viz/correlazione-municipi.png",
+  plot = correlazione_municipi,
+  width = 20,
+  height = 20,
+  units = "cm"  
+)
+
+ggsave(
+  "viz/correlazione-municipi.svg",
+  plot = correlazione_municipi,
+  width = 20,
+  height = 20,
+  units = "cm"  
+)
+
+
+# summarise density ---------------------------------------------
+
+# dat_pop <- 
+#   dat %>% 
+#   summarise(
+#     total_car_number_per_100m = total_car_number_per_100m %>% 
+#       mean(na.rm = T),
+#     average_denisty_of_population_per_100m2 = average_denisty_of_population_per_100m2 %>% 
+#       mean(na.rm = T),
+#     weight = n(),
+#     .by = name
+#   ) 
+# 
+# dat_pop %>%  
+#   uncount(weight) %>% 
+#   drop_na() %>% 
+#   {
+#     cor(
+#       .$total_car_number_per_100m,
+#       .$average_denisty_of_population_per_100m2,
+#       method = "spearman"
+#     )
+#   }
+# 
+# dat_pop %>% 
+#   uncount(weight) %>%
+#   filter(total_car_number_per_100m < 25) %>% 
+#   ggplot() +
+#   aes(x = average_denisty_of_population_per_100m2,
+#       y = total_car_number_per_100m) +
+#   geom_point(shape = 1,
+#              alpha = .1)
+# 
+# dat_pop %>% 
+#   left_join(vie_e_municipi) %>% 
+#   mutate(weight = weight*perc_municipio %>% round()) %>% 
+#   drop_na() %>% 
+#   uncount(weight) %>% 
+#   filter(average_denisty_of_population_per_100m2 < 5) %>% 
+#   ggplot() +
+#   aes(x = average_denisty_of_population_per_100m2,
+#       y = total_car_number_per_100m) +
+#   geom_point(shape = 1,
+#              alpha = .1) +
+#   scale_y_log10() +
+#   facet_wrap(facets = "municipio")
+# 
+
+# old -----------------------------------------------------------
+
+
+dat_cors <- 
+  dat %>% 
+  group_by(name) %>% 
+  summarise_all(~mean(., na.rm = T)) %>% 
+  ungroup()
 
 dat_cors %>% 
   select(-name, -id) %>% 
@@ -69,6 +216,7 @@ dat_cors %>%
   scale_x_log10()
 
 dat_cors %>% 
+  # filter(`total car number per 100m` > 0) %>% 
   select(`total car number per 100m`, `average denisty of population per 100m2`) %>% 
   filter(`average denisty of population per 100m2` <= 10) %>% 
   ggplot() +
@@ -78,3 +226,52 @@ dat_cors %>%
     shape = 1
   ) +
   scale_y_log10() 
+
+dat_cors %>% 
+  # filter(`total car number per 100m` > 0) %>% 
+  select(`total car number per 100m`, `average denisty of population per 100m2`) %>% 
+  filter(`average denisty of population per 100m2` <= 10) %>%
+  ggplot() +
+  aes(x = `average denisty of population per 100m2`,
+      y = `total car number per 100m`) +
+  geom_point(
+    shape = 1
+  ) +
+  scale_y_log10() 
+
+dat_cors %>% 
+  # filter(`total car number per 100m` > 0) %>% 
+  select(`total car number per 100m`, `sum of population in 100m buffer`) %>% 
+  # filter(`average denisty of population per 100m2` <= 10) %>% 
+  ggplot() +
+  aes(x =`sum of population in 100m buffer`,
+      y = `total car number per 100m`) +
+  geom_point(
+    shape = 1
+  ) +
+  scale_x_log10() +
+  scale_y_log10()
+
+dat_cors %>% 
+  filter(`total car number per 100m` > 0) %>%
+  select(`total car number per 100m`, `average denisty of population per 100m2`) %>% 
+  filter(`average denisty of population per 100m2` <= 10) %>%
+  drop_na() %>% 
+  {
+    cor(.[,1], .[,2], method = "spearman") 
+  }
+
+dat_cors %>% 
+  # filter(`total car number per 100m` > 0) %>%
+  select(`total car number per 100m`, `sum of population in 100m buffer`) %>% 
+  # filter(`average denisty of population per 100m2` <= 10) %>%
+  drop_na() %>% 
+  {
+    cor(.[,1], .[,2], method = "spearman") 
+  }
+
+dat_cors_muni <- 
+  dat_cors %>% 
+  clean_names() %>% 
+  left_join(vie_e_municipi) %>% 
+  select(name, municipio, perc_municipio, total_car_number_per_100m, sum_of_population_in_100m_buffer)
